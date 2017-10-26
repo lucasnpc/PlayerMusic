095791010dba13099c0064aa359d2b0155d7aa30
lucasnpc <31970919+lucasnpc@users.noreply.github.com>
2017-10-26 15:06:12 -0200

    Atualizado

--- app/src/main/java/com/example/cti/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/cti/musicplayer/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.cti.musicplayer;
 
+import android.content.ComponentName;
 import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.IBinder;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
 
     private ArrayList<Song> songList;
     private ListView songView;
+    private MusicService musicSrv;
+    private Intent playIntent;
+    private boolean musicBound = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +42,31 @@ public class MainActivity extends AppCompatActivity {
         });
         SongAdapter songAdt = new SongAdapter(this, songList);
         songView.setAdapter(songAdt);
+    }
+
+    private ServiceConnection musicConection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            MusicService.MusicBinder binder = (MusicService.MusicBinder)service;
+            musicSrv = binder.getService();
+            musicSrv.setList(songList);
+            musicBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            musicBound = false;
+        }
+    };
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(playIntent==null){
+            playIntent = new Intent(this,MusicService.class);
+            bindService(playIntent,musicConection, Context.BIND_AUTO_CREATE);
+            startService(playIntent);
+        }
     }
 
     public void getSongList(){
--- app/src/main/java/com/example/cti/musicplayer/MusicService.java
+++ app/src/main/java/com/example/cti/musicplayer/MusicService.java
@@ -1,10 +1,68 @@
 package com.example.cti.musicplayer;
 
 import android.app.Service;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Binder;
+import android.os.IBinder;
+import android.os.PowerManager;
+import android.support.annotation.Nullable;
+
+import java.util.ArrayList;
 
 /**
  * Created by Cti on 25/10/2017.
  */
-/*
-public class MusicService extends Service {
-*/
+
+public class MusicService extends Service implements MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener,
+MediaPlayer.OnCompletionListener{
+
+    private MediaPlayer player;
+    private ArrayList<Song> songs;
+    private int songPosn;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        songPosn=0;
+        player= new MediaPlayer();
+        initMusicPlayer();
+    }
+
+    public void initMusicPlayer(){
+        player.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
+        player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+    }
+
+      @Override
+    public void onCompletion(MediaPlayer mp) {
+
+    }
+
+    @Override
+    public boolean onError(MediaPlayer mp, int what, int extra) {
+        return false;
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+
+    }
+
+    public void setList(ArrayList<Song> theSongs){
+        songs = theSongs;
+    }
+
+    public class MusicBinder extends Binder{
+        MusicService getService(){
+            return MusicService.this;
+        }
+    }
+}
